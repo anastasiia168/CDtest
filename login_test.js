a38fb81d776181a3f785d534b7329a0bdda0a51a
Anastasiia Slesarenko <a.v.slesarenko@mail.ru>
2020-06-04 03:15:13 +0300

    .

--- login_test.js
+++ login_test.js
@@ -1,19 +1,20 @@
+
+
+
 var webdriver = require('selenium-webdriver'),
-By=webdriver.By,
-    until = webdriver.until;
+    chrome = require('selenium-webdriver/chrome');
 
-const assert = require ('assert');
-        driver = new webdriver.Builder()
-            .forBrowser('chrome').build();
-//chrome_driver = new webdriver.Builder().forBrowser('chrome').build();
-//ie_driver = new webdriver.Builder().forBrowser('ie').build();
-//firefox_driver = new webdriver.Builder().forBrowser('firefox').build();
+var options = new chrome.Options();
+options.addArguments(["start-maximized"]);
 
+driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
 
     it('Login', function() {
 
         driver.get("https://sagenda.net/");
-        driver.wait(until.titleIs('Login'), 10000);
         driver.findElement(By.name('email')).sendKeys('naska121990@mail.ru');
         driver.findElement(By.name('password')).sendKeys('1111');
         driver.findElement(By.name('login')).click();
