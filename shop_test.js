d9b3f185a2d3c3bac11e74e932c5c86d57592be5
Anastasiia Slesarenko <a.v.slesarenko@mail.ru>
2020-06-11 17:15:15 +0300

    Zadanie 8 . Ne nahoditsya sticker otnositel'no utki. Zadanie 11

--- shop_test.js
+++ shop_test.js
@@ -41,41 +41,7 @@ Keys = driver.Key,
                }
            });
 });
-    it
-    ("Registration1", async () => {
 
-        await  driver.get("https://litecart.stqa.ru/ru/");
-        await  driver.findElement(By.css('#box-account-login > div > form > table > tbody > tr:nth-child(5) > td > a')).click();
-        await driver.wait(until.elementLocated(By.name('tax_id'),1000));
-        await  driver.findElement(By.name('tax_id')).sendKeys('1111');
-        await driver.findElement(By.name('company')).sendKeys('ABS');
-        await driver.findElement(By.name('firstname')).sendKeys('Anna');
-        await driver.findElement(By.name('lastname')).sendKeys('Ivanova');
-        await driver.findElement(By.name('postcode')).sendKeys('11211');
-        await driver.findElement(By.name('city')).sendKeys('Chicago');
-        await driver.findElement(By.name('phone')).sendKeys('9871111');
-        await driver.findElement(By.name('password')).sendKeys(passwordRegistrationTest);
-        await driver.findElement(By.name('confirmed_password')).sendKeys(passwordRegistrationTest);
-        await  driver.findElement(By.css("span.select2")).click();
-        await driver.wait(until.elementLocated(By.className("select2-search__field"),5000));
-        await driver.findElement(By.className("select2-search__field")).sendKeys("United States"+Key.ENTER);
-        await driver.findElement(By.name('address1')).sendKeys('520 S Michigan Ave');
-        await driver.findElement(By.name('email')).sendKeys(emailRegistrationTest);
-        await driver.findElement(By.name('create_account')).click();
-        await driver.sleep (100);
-        await driver.findElement(By.linkText("Logout")).click();
-        console.log("logout");
-        await driver.wait(until.elementLocated(By.name('email'),1000));
-        await  driver.findElement(By.name('email')).sendKeys(emailRegistrationTest);
-        await driver.findElement(By.name('password')).sendKeys(passwordRegistrationTest);
-        await driver.findElement(By.name('login')).click();
-        console.log("login");
-        await driver.sleep (100);
-        const logout= driver.findElement(By.linkText("Logout"));
-        await  logout.click();
-        await driver.wait(until.elementLocated(By.name('email'),1000));
-        console.log("logout");
-    });
 
 
 
