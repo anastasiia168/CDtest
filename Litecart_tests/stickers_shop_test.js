31681aa1c482a00a3b783c68b9b9d336e488ad64
Anastasiia Slesarenko <a.v.slesarenko@mail.ru>
2020-06-23 10:18:32 +0300

    Stickers, Carttest,zonesSort

--- Litecart_tests/stickers_shop_test.js
+++ Litecart_tests/stickers_shop_test.js
@@ -10,18 +10,14 @@ const assert= require("assert");
 driver.manage().setTimeouts( { implicit: 4000 } );
     it
     ("Login", async () => {
-
         await   driver.get("http://localhost/litecart/en/");
         await  driver.findElement(By.name('email')).sendKeys('naska121990@mail.ru');
         await  driver.findElement(By.name('password')).sendKeys('1111');
         await  driver.findElement(By.name('login')).click();
         driver.findElement( By.css('#box-account > div > ul > li:nth-child(4) ')).click();
-
     });
  it
 ("DucksStikers", async () => {
-
-
         var ducks = await driver.findElements(By.className("product column shadow hover-light"));
     console.log(ducks.length);
     for(var item of ducks)
