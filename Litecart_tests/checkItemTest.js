eae3edca6341be3186f9aa479b825262947bf445
Anastasiia Slesarenko <a.v.slesarenko@mail.ru>
2020-06-28 04:21:57 +0300

    checkItemTest.js

--- Litecart_tests/checkItemTest.js
+++ Litecart_tests/checkItemTest.js
@@ -5,71 +5,77 @@ const {By, until} = require ("selenium-webdriver");
 const assert= require("assert");
 driver.manage().setTimeouts( { implicit: 3000 } );
 
-it ("main page", async () => {
-
-
+it ("Main page", async () => {
     await driver.get("https://litecart.stqa.ru/en/");
     await driver.wait(until.elementLocated(By.className('product column shadow hover-light')), 200);
-   await driver.wait(until.elementLocated(By.xpath('//*[@id="box-campaigns"]/div/ul/li/a[1]/div[2]')), 200);
-    var itemName = await driver.findElement(By.xpath('//*[@id="box-campaigns"]/div/ul/li/a[1]/div[2]'));
-    var itemNameContent = await itemName.getAttribute("textContent");
-    console.log(itemNameContent);
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="box-campaigns"]/div/ul/li/a[1]/div[2]')), 200);
+    global.itemName = await driver.findElement(By.xpath('//*[@id="box-campaigns"]/div/ul/li/a[1]/div[2]'));
+    global.itemNameContent = await itemName.getAttribute("textContent");
+    global.itemRegularPrice = await driver.findElement(By.css("#box-campaigns .regular-price"));
+    global.itemRegularPriceContent =  await itemRegularPrice.getAttribute("textContent");
+    global.itemRegularPriceColor =await itemRegularPrice.getCssValue("color");
+    let sep = await itemRegularPriceColor.indexOf(", ") > -1 ? ", " : " ";
+    RegularPrice =await itemRegularPriceColor.substr(5).split(")")[0].split(sep);
+    global.itemRegularPriceFontSize =await itemRegularPrice.getCssValue("font-size");
+    global.itemRegularPriceFontLine =await itemRegularPrice.getCssValue("text-decoration-line");
+    global.itemCampaignPrice = await driver.findElement(By.css("#box-campaigns .campaign-price"));
+    global.itemCampaignPriceContent =  await itemCampaignPrice.getAttribute("textContent");
+    global.itemCampaignPriceColor =  await itemCampaignPrice.getCssValue("color");
+    let sep1 =await itemRegularPriceColor.indexOf(", ") > -1 ? ", " : " ";
+    CampaignPrice = await itemCampaignPriceColor.substr(5).split(")")[0].split(sep1);
+    global.itemCampaignPriceFontSize =  await itemCampaignPrice.getCssValue("font-size");
+    global.itemCampaignPriceFontWeight =  await itemCampaignPrice.getCssValue("font-weight");
+});
 
-    var itemRegularPrice = await driver.findElement(By.css("#box-campaigns .regular-price"));
-    var itemRegularPriceContent =  await itemRegularPrice.getAttribute("textContent");
-    var itemRegularPriceColor =await itemRegularPrice.getCssValue("color");
-    var itemRegularPriceFont =await itemRegularPrice.getCssValue("font-size");
-    console.log(itemRegularPriceContent);
-    /*   var itemRegularPriceFontDouble = (float)convertT(itemRegularPriceFont);
-
-    //   var separators = { ",", "(", ")" };
-    var itemRegularPriceColorRGBA = itemRegularPriceColor.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-  //  var itemRegularPriceColorRGBATrim = myTrim(itemRegularPriceColorRGBA);
-*/
-    var itemCampaignPrice = await driver.findElement(By.css("#box-campaigns .campaign-price"));
-    var itemCampaignPriceContent =  await itemCampaignPrice.getAttribute("textContent");
-    var itemCampaignPriceColor =  await itemCampaignPrice.getCssValue("color");
-    var itemCampaignPriceFont =  await itemCampaignPrice.getCssValue("font-size");
-    console.log(itemCampaignPriceContent);
- //   var itemCampaignPriceColorRGBA = itemCampaignPriceColor.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-  //  var itemCampaignPriceColorRGBATrim = MyTrim(itemCampaignPriceColorRGBA);
-
-
-
-
-
-
-    var duck = await driver.findElement(By.css("#box-campaigns a.link"));
+ it ("Duck page", async () => {
+    var duck = await driver.findElement(By.css("#box-campaigns > div > ul > li > a.link"));
     duck.click();
-    console.log("1");
     await driver.wait(until.elementLocated(By.xpath('//*[@id="box-product"]/div[1]/h1'), 2000));
-    console.log("2");
-    var itemNameMain = await driver.findElement(By.css('#box-product > div:nth-child(1) > h1'));
-    var itemNameMainContent =  await itemNameMain.getAttribute("textContent");
-    console.log(itemNameMainContent);
-    var itemRegularPriceMain = await driver.findElement(By.css(".price-wrapper .regular-price"));
-    var itemRegularPriceMainContent = itemRegularPriceMain.getAttribute("textContent");
-    var itemRegularPriceMainFont = itemRegularPriceMain.getCssValue("font-size");
- //   var itemRegularPriceMainFontDouble = (float)Convert.ToDouble(itemRegularPriceMainFont);
-
-    var itemCampaignPriceMain =await  driver.findElement(By.css(".price-wrapper .campaign-price"));
-    var itemCampaignPriceMainContent = itemCampaignPriceMain.getAttribute("textContent");
-    var itemCampaignPriceMainFont = itemCampaignPriceMain.getCssValue("font-size");
+    global.itemNameMain = await driver.findElement(By.css('#box-product > div:nth-child(1) > h1'));
+    global.itemNameMainContent =  await itemNameMain.getAttribute("textContent");
+    global.itemRegularPriceMain = await driver.findElement(By.css(".price-wrapper .regular-price"));
+    global.itemRegularPriceMainContent = await itemRegularPriceMain.getAttribute("textContent");
+    global.itemRegularPriceMainColor =  await itemRegularPriceMain.getCssValue("color");
+    let sep2 = await itemRegularPriceMainColor.indexOf(", ") > -1 ? ", " : " ";
+    RegularPriceMain =await itemRegularPriceMainColor.substr(5).split(")")[0].split(sep2);
+    global.itemRegularPriceMainFontSize = await itemRegularPriceMain.getCssValue("font-size");
+    global.itemRegularPriceMainFontLine =await  itemRegularPriceMain.getCssValue("text-decoration-line");
+    global.itemCampaignPriceMain =await  driver.findElement(By.css(".price-wrapper .campaign-price"));
+    global.itemCampaignPriceMainContent = await itemCampaignPriceMain.getAttribute("textContent");
+    global.itemCampaignPriceMainColor =  await itemCampaignPriceMain.getCssValue("color");
+    sep3= await itemRegularPriceMainColor.indexOf(", ") > -1 ? ", " : " ";
+    CampaignPriceMain = itemCampaignPriceMainColor .substr(5).split(")")[0].split(sep3);
+    global.itemCampaignPriceMainFontSize =await  itemCampaignPriceMain.getCssValue("font-size");
+    global.itemCampaignPriceMainFontWeight =await  itemCampaignPriceMain.getCssValue("font-weight");
+ });
   //  var itemCampaignPriceMainFontDouble = (float)Convert.ToDouble(itemCampaignPriceMainFont);
+  it ("Duck's name ", async () => {
+    assert.ok(itemNameContent === itemNameMainContent, 'not equal Names');
+  });
 
-    assert.ok(itemNameContent == itemNameMainContent, 'not equal Names');
+   it ("Duck's price ", async () => {
     assert.ok(itemRegularPriceContent == itemRegularPriceMainContent, 'not equal Prices');
     assert.ok(itemCampaignPriceContent == itemCampaignPriceMainContent, 'not equal CampaignPrices');
-    /*
-      //  assert.areEqual(itemRegularPriceColorRGBATrim[1], itemRegularPriceColorRGBATrim[2]);
-      //  assert.areEqual(itemRegularPriceColorRGBATrim[2], itemRegularPriceColorRGBATrim[3]);
-      //  assert.areEqual(itemRegularPriceColorRGBATrim[1], itemRegularPriceColorRGBATrim[3]);
-
-     //   assert.areEqual(itemCampaignPriceColorRGBATrim[2], itemCampaignPriceColorRGBATrim[3]);
-     //   assert.areEqual(itemCampaignPriceColorRGBATrim[2], "0");
-     //   assert.areEqual(itemCampaignPriceColorRGBATrim[3], "0");
-
-       // assert.g    (itemCampaignPriceFontDouble, itemRegularPriceFontDouble);
-       // assert.Greater(itemCampaignPriceMainFontDouble, itemRegularPriceMainFontDouble);
-    */
-});
+  });
+ it ("Grey regular price ", async () => {
+     assert.ok(global.itemRegularPriceFontLine  == "line-through", 'not equal Prices');
+     assert.ok(itemRegularPriceMainFontLine == "line-through", 'not equal CampaignPrices');
+     assert.ok(RegularPriceMain [1] == RegularPriceMain [2] , 'not equal CampaignPrices');
+     assert.ok(RegularPriceMain [2] == RegularPriceMain [0] , 'not equal CampaignPrices');
+     assert.ok( RegularPrice[1] == RegularPrice[2], 'not equal CampaignPrices');
+     assert.ok( RegularPrice [2] == RegularPrice [0], 'not equal CampaignPrices');
+ });
+ it ("Red and bold action price ", async () => {
+   assert.ok(itemCampaignPriceMainFontWeight == "700", 'not equal Prices');
+   assert.ok(itemCampaignPriceFontWeight == "700", 'not equal CampaignPrices');
+   assert.ok(CampaignPriceMain [1] == 0 , 'not equal CampaignPrices');
+   assert.ok(CampaignPriceMain [2] == 0 , 'not equal CampaignPrices');
+   assert.ok(CampaignPriceMain [0] != 0 , 'not equal CampaignPrices');
+   assert.ok(CampaignPrice [1] == "0" , 'not equal CampaignPrices');
+   assert.ok(CampaignPrice [2] == "0" , 'not equal CampaignPrices');
+   assert.ok(CampaignPrice [0] != "0" , 'not equal CampaignPrices');
+ });
+ it ("Action price bigger than regular price ", async () => {
+  assert.ok(  itemRegularPriceMainFontSize < itemCampaignPriceMainFontSize, 'not equal Prices');
+  assert.ok(itemRegularPriceFontSize <itemCampaignPriceFontSize, 'not equal CampaignPrices');
+ });
